ec55fdfb350178f050d9811f351c2dd5dd0e2a7e
tharanga_madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-04-15 22:03:36 +0530

    Start to make second section

--- src/Containers/VarietyBar/VarietyBar.js
+++ src/Containers/VarietyBar/VarietyBar.js
@@ -1,17 +1,42 @@
 import React from 'react'
 import './VarietyBar.css';
 
+const clickChange = document.querySelectorAll('.click-change');
+
+clickChange.forEach((colorChange) => {
+  colorChange.addEventListener('click', () => {
+    document.querySelector('.after-clicked')?.classList.remove('after-clicked');
+    colorChange.classList.add('after-clicked');
+  })
+})
+
 const VarietyBar = () => {
   return (
     <div className='varietyBar'>
-      <div>
-        <ul className='varietyBar__varations'>
-          <li>Movies</li>
-          <li>TV Shows</li>
-          <li>Top Rated</li>
-          <li>Popular</li>
-          <li>Upcoming</li>
-        </ul>
+      <div className='varietyBar__outer'>
+        <div className='varietyBar__varations'>
+          <div>
+            <h6 className='click-change'>Movies</h6>
+            <div className='div-redBar varietyBar__redBar'></div>
+          </div>
+          <div>
+            <h6 className='click-change' >TV Shows</h6>
+            <div className='div-redBar'></div>
+          </div>
+          <div>
+            <h6 className='click-change' >Top Rated</h6>
+            <div className='div-redBar'></div>
+          </div>
+          <div>
+            <h6 className='click-change'>Popular</h6>
+            <div className='div-redBar'></div>
+          </div>
+          <div>
+            <h6 className='click-change'>Upcoming</h6>
+            <div className='div-redBar'></div>
+          </div>
+        </div>
+        
       </div>
     </div>
   )
