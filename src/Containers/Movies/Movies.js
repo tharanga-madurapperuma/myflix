c2cf5a9d17f7c53cd4a7d8e02f93c3f3029fdbda
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-12-26 07:47:55 +0530

    delete unwated folders

--- src/Containers/Movies/Movies.js
+++ src/Containers/Movies/Movies.js
@@ -1,29 +1,12 @@
-import React from 'react'
-import './Movies.css';
-import VarietyBar from '../VarietyBar/VarietyBar';
-import WhiteLine from '../../Components/WhiteLine/WhiteLine';
-import Genres from '../Genres/Genres';
-import Posters from '../Posters/Posters';
-import Scroll from '../Scroll/Scroll';
-import Suggestions from '../Suggestions/Suggestions';
-import Footer from '../Footer/Footer';
+import React from "react";
+import "./Movies.css";
 
 const Movies = () => {
-  return (
-    <div className='movie'>
-        <div className='secondSection'>
-            <VarietyBar />
-            <WhiteLine />
-            <Genres />
-            <Posters />
-            <Scroll />
-            <WhiteLine />
-            <Suggestions />
-            <WhiteLine />
-            <Footer />  
+    return (
+        <div className="movie">
+            <div className="secondSection"></div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Movies
+export default Movies;
