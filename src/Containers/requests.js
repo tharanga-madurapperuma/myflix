579720a1f7ad4e98dec32565804bcc339190f585
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-12-26 10:55:49 +0530

    add trending url to requests

--- src/Containers/requests.js
+++ src/Containers/requests.js
@@ -1,39 +1,25 @@
 const api_key = "402ab982f69b1e475e37d7f951d28493";
-
-// Get genres, returns ID of genre
-//https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}
-
-//Popular
-// https://api.themoviedb.org/3/movie/popular?api_key=${api_key}
-
-//Now Playing
-//https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}
-
-// Top Rated
-//https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}
-
-//Upcoming
-//https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}
+const BASE_URL = "https://api.themoviedb.org/3";
 
 const requests = {
-    fetchAction: `/discover/movie?api_key=${api_key}&with_genres=27`,
-    fetchAdventure: `/discover/movie?api_key=${api_key}&with_genres=12`,
-    fetchAnimation: `/discover/movie?api_key=${api_key}&with_genres=16`,
-    fetchComedy: `/discover/movie?api_key=${api_key}&with_genres=35`,
-    fetchCrime: `/discover/movie?api_key=${api_key}&with_genres=80`,
-    fetchDocimentary: `/discover/movie?api_key=${api_key}&with_genres=99`,
-    fetchDrama: `/discover/movie?api_key=${api_key}&with_genres=18`,
-    fetchFamily: `/discover/movie?api_key=${api_key}&with_genres=10751`,
-    fetchFantasy: `/discover/movie?api_key=${api_key}&with_genres=14`,
-    fetchHistory: `/discover/movie?api_key=${api_key}&with_genres=36`,
-    fetchHorror: `/discover/movie?api_key=${api_key}&with_genres=27`,
-    fetchMusic: `/discover/movie?api_key=${api_key}&with_genres=10402`,
-    fetchMystery: `/discover/movie?api_key=${api_key}&with_genres=9648`,
-    fetchRomance: `/discover/movie?api_key=${api_key}&with_genres=10749`,
-    fetchScienceFiction: `/discover/movie?api_key=${api_key}&with_genres=878`,
-    fetchTVMovie: `/discover/movie?api_key=${api_key}&with_genres=10770`,
-    fetchWar: `/discover/movie?api_key=${api_key}&with_genres=10752`,
-    fetchWestern: `/discover/movie?api_key=${api_key}&with_genres=37`,
+    fetchAction: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=27`,
+    fetchTrending: `${BASE_URL}/movie/now_playing?api_key=${api_key}&language=en-US`,
+    fetchAdventure: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=12`,
+    fetchAnimation: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=16`,
+    fetchComedy: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=35`,
+    fetchCrime: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=80`,
+    fetchDocimentary: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=99`,
+    fetchDrama: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=18`,
+    fetchFamily: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=10751`,
+    fetchFantasy: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=14`,
+    fetchHistory: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=36`,
+    fetchHorror: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=27`,
+    fetchMusic: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=10402`,
+    fetchRomance: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=10749`,
+    fetchScienceFiction: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=878`,
+    fetchTVMovie: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=10770`,
+    fetchWar: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=10752`,
+    fetchWestern: `${BASE_URL}/discover/movie?api_key=${api_key}&with_genres=37`,
 };
 
 const action = {
