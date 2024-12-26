ac72025722586e39bbaba1a2cc14fb6c3ea6cd3e
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-12-26 21:05:51 +0530

    change the routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import './App.css';
-import Home from './Containers/Home/Home.js';
-import Movies from './Containers/Movies/Movies.js';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
+import "./App.css";
+import Home from "./Containers/Home/Home.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NowPlaying from "./Containers/NowPlaying/NowPlaying.js";
+import Movies from "./Containers/Movies/Movies.js";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='movies' element={<Movies/>} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/nowPlaying" element={<NowPlaying />} />
+                    <Route path="/movies" element={<Movies />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
